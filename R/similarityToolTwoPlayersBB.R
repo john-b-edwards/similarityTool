e404343c2cd105622704d4aa4df35aeeb93792f0
Metlover <edwards1860@gmail.com>
2017-11-30 16:28:46 -0500

    removed documentation

--- R/similarityToolTwoPlayersBB.R
+++ R/similarityToolTwoPlayersBB.R
@@ -1,14 +1,3 @@
-#' Batted Ball Similarity Tool (Two Players)
-#'
-#' Finds the similarity score of two players based solely upon batted bal data.
-#' @param battedBallVals A dataframe of Fangraphs batted ball data. Must be provided by user.
-#' @param playerNameOne A string of a player's name EXACTLY how it is recorded by Fangraphs
-#' @param playerNameTwo A string of a player's name EXACTLY how it is recorded by Fangraphs
-#' @return dataframe of the most similar players to the specified player and their similarity scores. Scores are relative to the sample, and will not be the same regardless of sample.
-#' @keywords similarityTool
-#' @export
-#' @examples
-#' similarityToolTwoPlayersBB()
 similarityToolTwoPlayersBB <- function(battedBallVals, playerNameOne, playerNameTwo){
   source("R/similarityToolBB.R")
   length = nrow(battedBallVals)
