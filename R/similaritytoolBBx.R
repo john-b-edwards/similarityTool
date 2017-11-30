7f3f40a4cf8f412a61f04f1c1c1b2b055c800079
Metlover <edwards1860@gmail.com>
2017-11-30 16:29:25 -0500

    removed documentation

--- R/similaritytoolBBx.R
+++ R/similaritytoolBBx.R
@@ -1,15 +1,3 @@
-#' Batted Ball Similarity Tool (Player Unspecified)
-#'
-#' Finds the most similar players to a given player not in a dataset based solely upon batted ball data.
-#' @param battedBallVals A dataframe of Fangraphs batted ball data. Must be provided by user.
-#' @param playerVals A dataframe of a specified player's batted ball data. I.e. PlayerVals = data.frame("LD%" = .xxx, "GB%" = .xxx, "FB%" = .xxx, "Pull%" = .xxx, "Cent%" = .xxx, "Oppo%" = .xxx, "Soft%" = .xxx, "Med%" = .xxx, "Hard%" = .xxx, "HR/FB" = .xxx, check.names=FALSE)
-#' @param numResults Determines how many players who are alike to the specified player are returned. Defaults to 10.
-#' @return dataframe of the most similar players to the specified player and their similarity scores. Scores are relative to the sample, and will not be the same regardless of sample.
-#' @keywords similarityTool
-#' @export
-#' @examples
-#' similarityToolBBx()
-
 similarityToolBBx <- function(battedBallVals, playerVals, numResults=10){
   numResults = numResults + 1 
   names = battedBallVals[,"Name"] 
