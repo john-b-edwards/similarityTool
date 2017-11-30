f8227058d3d51688b3be9cc0c3ac2c19ca73b470
Metlover <edwards1860@gmail.com>
2017-11-30 16:27:46 -0500

    removed documentation

--- R/similarityToolMostUniqueOff.R
+++ R/similarityToolMostUniqueOff.R
@@ -1,14 +1,3 @@
-#' Most Unique Players Tool (Offense)
-#'
-#' Finds the players who have the most unique offensive peripheral data in a given offensive peripheral set.
-#' @param battedBallVals A dataframe of Fangraphs offensive peripheral data. Must be provided by user.
-#' @param numResults Determines how many players who are alike to the specified player are returned. Defaults to 10.
-#' @return dataframe of the most similar players to the specified player and their similarity scores. Scores are relative to the sample, and will not be the same regardless of sample.
-#' @keywords similarityTool
-#' @export
-#' @examples
-#' similarityToolMostUniqueOff()
-
 similarityToolMostSimOff <- function(battedBallVals, numResults){
   if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
   source("R/similarityToolOff.R")
