1df7a5cb086b2e60b7f8cef7d8b4b24679722996
Metlover <edwards1860@gmail.com>
2017-12-01 19:03:31 -0500

    Update similarityToolMostUniqueOff.R

--- R/similarityToolMostUniqueOff.R
+++ R/similarityToolMostUniqueOff.R
@@ -1,13 +1,11 @@
 similarityToolMostUniqueOff <- function(battedBallVals, numResults){
   if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
-  source("similarityToolOff.R")
   names = battedBallVals[,"Name"]
   result = data.frame(NULL)
   for (row in 1:nrow(names)) {
     name = names[row,]
     result = rbind(result, similarityToolOff(battedBallVals, toString(name), 1))
   }
-  result = result[seq(1,nrow(result)-1,2),]
   result$OriginalPlayer = as.data.frame(names)
   names(result)[names(result)=="Name"] <- "Most Similar Player"
   result = result[order(-result$Scores),]
