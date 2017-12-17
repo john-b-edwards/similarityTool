e77429b92d8c6691448311e269fe1e9fb93d310f
Metlover <edwards1860@gmail.com>
2017-12-17 01:31:05 -0500

    fixed methodology for scores across seasons

--- R/similarityToolMostUniqueOff.R
+++ R/similarityToolMostUniqueOff.R
@@ -1,10 +1,11 @@
 similarityToolMostUniqueOff <- function(battedBallVals, numResults){
   if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
+  battedBallVals$Name = paste(battedBallVals$Name, as.character(battedBallVals$Season), sep='')
   names = battedBallVals[,"Name"]
   result = data.frame(NULL)
   for (row in 1:nrow(names)) {
     name = names[row,]
-    result = rbind(result, similarityToolOff(battedBallVals, toString(name), 1))
+    result = rbind(result, similarityToolOff(battedBallVals, substr(toString(name),0,nchar(toString(name))-4), 1, strtoi(substr(toString(name),nchar(toString(name))-3,nchar(toString(name))))))
   }
   result$OriginalPlayer = as.data.frame(names)
   names(result)[names(result)=="Name"] <- "Most Similar Player"
