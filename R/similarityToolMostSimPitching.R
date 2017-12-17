13eb7832ae33a5aa64bb64ee14e5094c33fcf4e5
Metlover <edwards1860@gmail.com>
2017-12-17 15:22:20 -0500

    Create similarityToolMostSimPitching.R

---
+++ R/similarityToolMostSimPitching.R
@@ -0,0 +1,15 @@
+similarityToolMostSimPitching <- function(battedBallVals, numResults){
+  if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
+  battedBallValsTwo = battedBallVals
+  battedBallValsTwo$Name = paste(battedBallVals$Name, as.character(battedBallVals$Season), sep='')
+  names = battedBallValsTwo[,"Name"]
+  result = data.frame(NULL)
+  for (row in 1:nrow(names)) {
+    name = names[row,]
+    result = rbind(result, similarityToolPitching(battedBallVals, substr(toString(name),0,nchar(toString(name))-4), 1, as.double(substr(toString(name),nchar(toString(name))-3,nchar(toString(name))))))
+  }
+  result$OriginalPlayer = as.data.frame(names)
+  names(result)[names(result)=="Name"] <- "Most Similar Player"
+  result = result[order(result$Scores),]
+  return(result[1:numResults,])
+}
