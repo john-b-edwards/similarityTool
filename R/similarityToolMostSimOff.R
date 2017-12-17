fffa61fde33068bbadfe9afc2a1cdedffb969930
Metlover <edwards1860@gmail.com>
2017-12-17 14:38:31 -0500

    Update similarityToolMostSimOff.R

--- R/similarityToolMostSimOff.R
+++ R/similarityToolMostSimOff.R
@@ -1,14 +1,15 @@
 similarityToolMostSimOff <- function(battedBallVals, numResults){
   if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
-  battedBallVals$NameTwo = paste(battedBallVals$Name, as.character(battedBallVals$Season), sep='')
-  names = battedBallVals[,"NameTwo"]
+  battedBallValsTwo = battedBallVals
+  battedBallValsTwo$Name = paste(battedBallVals$Name, as.character(battedBallVals$Season), sep='')
+  names = battedBallValsTwo[,"Name"]
   result = data.frame(NULL)
   for (row in 1:nrow(names)) {
     name = names[row,]
     result = rbind(result, similarityToolOff(battedBallVals, substr(toString(name),0,nchar(toString(name))-4), 1, as.double(substr(toString(name),nchar(toString(name))-3,nchar(toString(name))))))
   }
   result$OriginalPlayer = as.data.frame(names)
-  names(result)[names(result)=="NameTwo"] <- "Most Similar Player"
+  names(result)[names(result)=="Name"] <- "Most Similar Player"
   result = result[order(result$Scores),]
   return(result[1:numResults,])
 }
