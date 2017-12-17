a5d45b2ec91951b90f55d6334035083e5679bc9b
Metlover <edwards1860@gmail.com>
2017-12-17 01:11:37 -0500

    updated to function with multiple seasons

--- R/similarityToolMostSimBB.R
+++ R/similarityToolMostSimBB.R
@@ -1,10 +1,12 @@
 similarityToolMostSimBB <- function(battedBallVals, numResults){
   if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
   names = battedBallVals[,"Name"]
+  years = battedBallVals[,"Season"]
   result = data.frame(NULL)
   for (row in 1:nrow(names)) {
     name = names[row,]
-    result = rbind(result, similarityToolBB(battedBallVals, toString(name), 1))
+    year = years[row,]
+    result = rbind(result, similarityToolBB(battedBallVals, toString(name), 1, year))
   }
   result$OriginalPlayer = as.data.frame(names)
   names(result)[names(result)=="Name"] <- "Most Similar Player"
