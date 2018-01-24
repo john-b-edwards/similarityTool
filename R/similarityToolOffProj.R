53f293644c63c069f543f98be37b0b04aadb57c1
Metlover <edwards1860@gmail.com>
2018-01-24 12:44:52 -0500

    created similarity tool for projection system

---
+++ R/similarityToolOffProj.R
@@ -0,0 +1,12 @@
+similarityToolOffProj <- function(battedBallVals, playerVals, numResults=10, year=2017){
+  require(FNN)
+  numResults = numResults + 1
+  names = battedBallVals[,"Name"] 
+  if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1} 
+  convertedVals = data.frame(rbind(as.matrix(battedBallVals), as.matrix(playerVals))) 
+  bbvNorm <- scale(convertedVals) 
+  results = get.knnx(bbvNorm, as.data.frame(t(bbvNorm[nrow(bbvNorm),])), k=numResults+1, algorithm="kd_tree") 
+  resultingNames = names[results$nn.index[c(2:numResults+1)],] 
+  endResult = data.frame ("Names" = resultingNames, "Scores" = results$nn.dist[c(2:numResults+1)], stringsAsFactors = FALSE)
+  return(endResult) 
+}
