ae04c9b955117846cca140398dc4df4d430b36e1
Metlover <edwards1860@gmail.com>
2018-01-24 12:55:38 -0500

    Update similarityToolOffProj.R

--- R/similarityToolOffProj.R
+++ R/similarityToolOffProj.R
@@ -2,8 +2,9 @@ similarityToolOffProj <- function(battedBallVals, playerVals, numResults=10, yea
   require(FNN)
   numResults = numResults + 1
   names = battedBallVals[,"Name"] 
-  if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1} 
-  convertedVals = data.frame(rbind(as.matrix(battedBallVals), as.matrix(playerVals))) 
+  if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
+  convertedVals<- battedBallVals[,c('LD%','GB%','FB%','Pull%','Cent%','Oppo%','Soft%','Med%','Hard%', 'HR/FB', 'K%', 'BB%')]
+  convertedVals = data.frame(rbind(as.matrix(convertedVals), as.matrix(playerVals))) 
   bbvNorm <- scale(convertedVals) 
   results = get.knnx(bbvNorm, as.data.frame(t(bbvNorm[nrow(bbvNorm),])), k=numResults+1, algorithm="kd_tree") 
   resultingNames = names[results$nn.index[c(2:numResults+1)],] 
