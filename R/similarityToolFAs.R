d761e5a82c3c8f5bdcaabc25c1a890b60b68387f
Metlover <edwards1860@gmail.com>
2018-02-05 23:48:53 -0500

    Create similarityToolFAs.R

---
+++ R/similarityToolFAs.R
@@ -0,0 +1,13 @@
+similarityToolOffProj <- function(battedBallVals, playerVals, numResults=10){
+  require(FNN)
+  numResults = numResults + 1
+  names = battedBallVals[,"Name"] 
+  if(numResults > nrow(battedBallVals)) {numResults = nrow(battedBallVals) - 1}
+  convertedVals<- battedBallVals[,c('AGE','WAR','CAREERWAR')]
+  convertedVals = data.frame(rbind(as.matrix(convertedVals), as.matrix(playerVals))) 
+  bbvNorm <- scale(convertedVals) 
+  results = get.knnx(bbvNorm, as.data.frame(t(bbvNorm[nrow(bbvNorm),])), k=numResults+1, algorithm="kd_tree") 
+  resultingNames = names[results$nn.index[c(2:numResults+1)],] 
+  endResult = data.frame ("Names" = resultingNames, "Scores" = results$nn.dist[c(2:numResults+1)], stringsAsFactors = FALSE)
+  return(endResult) 
+}
